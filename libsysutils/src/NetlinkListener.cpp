8a6286d15509b17c164c997d22d0c2fa74b158d4
RealJohnGalt <johngaltfirstrun@gmail.com>
2016-07-06 13:49:39 -0700

    libsysutils: quiet down some logging
    this is to be expected for now, so quiet down the logging a bit.
    Change-Id: If6d9daf41bc1d23940c88a6ed01f0e2e937c6010

--- libsysutils/src/NetlinkListener.cpp
+++ libsysutils/src/NetlinkListener.cpp
@@ -52,7 +52,7 @@ bool NetlinkListener::onDataAvailable(SocketClient *cli)
     if (count < 0) {
         if (uid > 0)
             LOG_EVENT_INT(65537, uid);
-        SLOGE("recvmsg failed (%s)", strerror(errno));
+        //SLOGE("recvmsg failed (%s)", strerror(errno));
         return false;
     }
 
